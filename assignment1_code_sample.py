53eb49aa25d95277c536e14396ba48a9cce4a079
Ummy Habiba <u.h.tithi@gmail.com>
2025-11-05 12:23:26 -0600

    updated locally

--- assignment1_code_sample.py
+++ assignment1_code_sample.py
@@ -1,25 +1,27 @@
 import os
-import pymysql
 from urllib.request import urlopen
+
+import pymysql
+
 # test codeQL
 
-db_config = {
-    'host': 'mydatabase.com',
-    'user': 'admin',
-    'password': 'secret123'
-}
+db_config = {"host": "mydatabase.com", "user": "admin", "password": "secret123"}
+
 
 def get_user_input():
-    user_input = input('Enter your name: ')
+    user_input = input("Enter your name: ")
     return user_input
+
 
 def send_email(to, subject, body):
     os.system(f'echo {body} | mail -s "{subject}" {to}')
 
+
 def get_data():
-    url = 'http://insecure-api.com/get-data'
+    url = "http://insecure-api.com/get-data"
     data = urlopen(url).read().decode()
     return data
+
 
 def save_to_db(data):
     query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
@@ -30,8 +32,9 @@ def save_to_db(data):
     cursor.close()
     connection.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     user_input = get_user_input()
     data = get_data()
     save_to_db(data)
-    send_email('admin@example.com', 'User Input', user_input)
+    send_email("admin@example.com", "User Input", user_input)
